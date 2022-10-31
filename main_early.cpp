bd299aab6163429ada2baa989e20a1fdfa09bc3f
eXtreme4ik <72621959+eXtreme4ik@users.noreply.github.com>
2022-11-01 01:25:45 +0500

    Add files via upload

--- main_early.cpp
+++ main_early.cpp
@@ -20,7 +20,7 @@
 #define LED_TOTAL 82   // Общее количество светодиодов в Ёлке
 #define LED_STAR 10    // Количество светодиодов звезды (с обоих сторон последовательно)
 #define LED_4STRIPE 18 // (4+4+3+3+2+2) Количество светодиодов в одной из 4-х полосок
-#define LED_1STRIPE 40 // Количество светодиодов в одной полоске (4 полоски последовательно) (TEST)
+#define LED_1STRIPE 40  // Количество светодиодов в одной полоске (4 полоски последовательно) (TEST)
 //#define LED_1STRIPE 72  // Количество светодиодов в одной полоске (4 полоски последовательно)
 #ifdef ONE_STRIPE
 #define STRIPE_PIN 5 // Пин ленты
@@ -71,10 +71,9 @@
 #define BUTTON_MODE SW2
 #define BUTTON_HIDDEN SW4
 
-#define MODE_PROGRAMM 0
 #define MODE_BRIGHTLESS 1
-#define MODE_SPEED 2
-#define MAXMODES 3
+#define MODE_PROGRAMM 0
+#define MAXMODES 2
 
 //----------------------------EXAMPLE FOR MACRO----------------------\/
 //
@@ -99,8 +98,7 @@
 Adafruit_NeoPixel stripe(LED_1STRIPE, STRIPE_PIN, TYPELED + LEDSPEED);
 Adafruit_NeoPixel star(LED_STAR, STAR_PIN, TYPELED + LEDSPEED);
 #define BRIGHT_MAX 100
-#define SPEED_MAX 10
-#define BRIGHT_STEP BRIGHT_MAX / 10
+#define BRIGHT_STEP BRIGHT_MAX / LED_1STRIPE
 #endif
 #ifdef FOUR_STRIPES
 Adafruit_NeoPixel stripe1(LED_4STRIPE, STRIPE0_PIN, TYPELED + LEDSPEED);
@@ -114,21 +112,19 @@ Adafruit_NeoPixel star(LED_STAR, STAR_PIN, TYPELED + LEDSPEED);
 void setupInterrupt(void);
 void setupStripe(Adafruit_NeoPixel &strip, uint8_t brightness);
 void allProgramms(Adafruit_NeoPixel &strip);
-void roundPixel(uint32_t color, int wait, Adafruit_NeoPixel &strip);
-void statefunction(Adafruit_NeoPixel &strip);
-int firstPixelHue = 0;
-uint16_t roundcount = 0;
+void randomPixel(int wait, int rounds, Adafruit_NeoPixel &strip, bool clear = 0);
+void roundPixel(uint32_t color, int wait, int rounds, Adafruit_NeoPixel &strip);
+void rainbow(int wait, Adafruit_NeoPixel &strip);
+void theaterChase(uint32_t color, int wait, Adafruit_NeoPixel &strip);
+void colorWipe(uint32_t color, int wait, Adafruit_NeoPixel &strip);
 int timeButton = 0;
 int mode = 0;
 int bright = 50;
 int prog = 0;
-int speed = 1;
+int speed = 50;
 bool up = false;
 bool dn = false;
 bool md = false;
-bool flag = false;
-int pixel = 0;
-int r, g, b = 0;
 ISR(TIMER1_OVF_vect)
 {
   int value = analogRead(BUTTONS_ADC_PIN);
@@ -140,24 +136,20 @@ ISR(TIMER1_OVF_vect)
     if (value == BUTTON_HIDDEN)
     {
       timeButton = 100;
-      flag = true;
     }
     if (value == BUTTON_UP)
     {
       up = true;
-      flag = true;
       timeButton = 100;
     }
     if (value == BUTTON_DOWN)
     {
       dn = true;
-      flag = true;
       timeButton = 100;
     }
     if (value == BUTTON_MODE)
     {
       md = true;
-      flag = true;
       timeButton = 100;
     }
   }
@@ -198,23 +190,10 @@ ISR(TIMER1_OVF_vect)
       prog -= 1;
       dn = false;
     }
-  }
-  if (mode == MODE_SPEED)
-  {
-    if (up)
-    {
-      speed -= 1;
-      up = false;
-    }
-    if (dn)
-    {
-      speed += 1;
-      dn = false;
-    }
-    if (speed < 1)
-      speed = 1;
-    if (speed > SPEED_MAX)
-      speed = SPEED_MAX;
+    if (prog < 0)
+      prog = 3;
+    if (prog > 3)
+      prog = 0;
   }
 }
 void setup()
@@ -237,9 +216,47 @@ void setup()
 void loop()
 {
   stripe.setBrightness(bright);
-  statefunction(stripe);
-}
+  switch (mode)
+  {
+  case MODE_PROGRAMM:
+    switch (prog)
+    {
+    case 0:
+      randomPixel(50, 5, stripe, true);
+      break;
+    case 1:
+      roundPixel(stripe.Color(255, 0, 0), 50, 1, stripe);
+      roundPixel(stripe.Color(0, 255, 0), 50, 1, stripe);
+      roundPixel(stripe.Color(0, 0, 255), 50, 1, stripe);
+      break;
+    case 2:
+      randomPixel(50, 50, stripe, false);
+      break;
+    case 3:
+      rainbow(10, stripe);
+      break;
+    }
+    break;
+  case MODE_BRIGHTLESS:
+    int a = BRIGHT_STEP;
+    for (uint16_t b = 0; b < stripe.numPixels(); b++)
+    {
+      if (a < bright)
+      {
+        stripe.setPixelColor(b, stripe.Color(100, 100, 100));
+        a += BRIGHT_STEP;
+      }
+      if (a > bright)
+      {
+        stripe.setPixelColor(b, stripe.Color(0, 0, 0));
+        a -= BRIGHT_STEP;
+      }
+    }
 
+    stripe.show();
+    break;
+  }
+}
 void setupInterrupt(void)
 {
   cli();
@@ -257,135 +274,130 @@ void setupStripe(Adafruit_NeoPixel &strip, uint8_t brightness)
   strip.show();
   strip.setBrightness(brightness);
 }
-// Some functions of our own for creating animated effects -----------------
-void roundPixel(uint32_t color, int wait, Adafruit_NeoPixel &strip)
+void allProgramms(Adafruit_NeoPixel &strip)
 {
-  if (roundcount > strip.numPixels())
-    roundcount = 0;
-  strip.setPixelColor(roundcount, color);
-  strip.setPixelColor(roundcount - 1, strip.Color(0, 0, 0));
-  strip.show();
-  delay(wait);
-  roundcount++;
+  rainbow(10, strip);
+  strip.clear();
+  delay(500);
+  roundPixel(strip.Color(255, 0, 0), 100, 1, strip);
+  roundPixel(strip.Color(0, 255, 0), 100, 1, strip);
+  roundPixel(strip.Color(0, 0, 255), 100, 1, strip);
+  roundPixel(strip.Color(255, 0, 255), 100, 1, strip);
+  strip.clear();
+  delay(500);
+  randomPixel(100, 50, strip, true);
+  strip.clear();
+  delay(500);
+}
+// Some functions of our own for creating animated effects -----------------
+void randomPixel(int wait, int rounds, Adafruit_NeoPixel &strip, bool clear)
+{
+  for (int i = 0; i < rounds; i++)
+  {
+    if (clear)
+      strip.clear();
+    char colR = random();
+    char colG = random();
+    char colB = random();
+    uint8_t num = random();
+    while (num > strip.numPixels())
+    {
+      num = random();
+    }
+    strip.setPixelColor(num, strip.Color(colR, colG, colB));
+    strip.show();
+    delay(wait);
+  }
+  strip.clear();
+}
+void roundPixel(uint32_t color, int wait, int rounds, Adafruit_NeoPixel &strip)
+{
+  for (int n = 0; n < rounds; n++)
+  {
+    for (uint16_t i = 0; i <= strip.numPixels(); i++)
+    {
+      strip.setPixelColor(i, color);
+      strip.setPixelColor(i - 1, strip.Color(0, 0, 0));
+      strip.show();
+      delay(wait);
+    }
+  }
+}
+void colorWipe(uint32_t color, int wait, Adafruit_NeoPixel &strip)
+{
+  for (uint16_t i = 0; i < strip.numPixels(); i++)
+  {                                // For each pixel in strip...
+    strip.setPixelColor(i, color); //  Set pixel's color (in RAM)
+    strip.show();                  //  Update strip to match
+    delay(wait);                   //  Pause for a moment
+  }
 }
 
-void statefunction(Adafruit_NeoPixel &strip)
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait, Adafruit_NeoPixel &strip)
 {
-  switch (mode)
-  {
-
-  case MODE_PROGRAMM:
-  {
-    uint32_t colRand = strip.gamma32(strip.ColorHSV((uint16_t)random()));
-    uint8_t num = random();
-    switch (prog)
-    {
-
-    case 0: // RANDOM CLEAR
-    {
-      while (num > strip.numPixels())
+  for (int a = 0; a < 10; a++)
+  { // Repeat 10 times...
+    for (int b = 0; b < 3; b++)
+    {                //  'b' counts from 0 to 2...
+      strip.clear(); //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for (uint16_t c = b; c < strip.numPixels(); c += 3)
       {
-        num = random();
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
       }
-      strip.setPixelColor(num, colRand);
-      strip.show();
-      delay(50*speed);
-      strip.clear();
-    }
-    break;
-
-    case 1: // RANDOM NON CLEAR
-    {
-      while (num > strip.numPixels())
-      {
-        num = random();
-      }
-      strip.setPixelColor(num, colRand);
-      strip.show();
-      delay(50*speed);
-    }
-    break;
-
-    case 2: // RAINBOW
-    {
-      for (int b = 0; b < 3; b++)
-      {
-        for (uint16_t c = b; c < strip.numPixels(); c += 3)
-        {
-          uint16_t hue = firstPixelHue + c * 65536L / strip.numPixels();
-          uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-          strip.setPixelColor(c, color);                       // Set pixel 'c' to value 'color'
-        }
-        strip.show();                // Update strip with new contents
-        firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-      }
-      delay(10*speed);
-    }
-    break;
-
-    case 3: // ROUNDPIXEL
-    {
-      roundPixel(colRand, 10*speed, strip);
-    }
-    break;
-    case 4: // SMALL SNOW
-    {
-      for (int i = 0; i < 5; i++)
-      {
-        uint8_t snows = random(100);
-        num = random();
-        while (num > strip.numPixels())
-        {
-          num = random();
-        }
-        strip.setPixelColor(num, strip.Color(snows, snows, snows));
-      }
-      strip.show();
-      delay(100*speed);
-      strip.clear();
-    }
-    break;
-    case 5: // SNOW STORM
-    {
-      for (int b = 0; b < 3; b++)
-      {
-        for (uint16_t c = b; c < strip.numPixels(); c += 3)
-        {
-          uint8_t hue = random(100);
-          uint32_t color = strip.Color(hue,hue,hue); // hue -> RGB
-          strip.setPixelColor(c, color);                       // Set pixel 'c' to value 'color'
-        }
-        strip.show();                // Update strip with new contents
-        firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-      }
-      delay(30*speed);
-    }
-    break;
-    default:
-      prog = 0;
-      break;
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
     }
   }
-  break;
+}
 
-  case MODE_BRIGHTLESS:
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait, Adafruit_NeoPixel &strip)
+{
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256)
   {
-    stripe.clear();
-    stripe.setPixelColor(0, stripe.Color(255, 255, 255));
-    stripe.show();
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    strip.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
   }
-  break;
+}
 
-  case MODE_SPEED:
-  {
-    stripe.clear();
-    delay(100*speed);
-    stripe.setPixelColor(0, stripe.Color(255, 255, 255));
-    stripe.show();
-    delay(100*speed);
-    stripe.setPixelColor(0, stripe.Color(0, 0, 0));
-    stripe.show();
-  }
-  break;
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait, Adafruit_NeoPixel &strip)
+{
+  int firstPixelHue = 0; // First pixel starts at red (hue 0)
+  for (int a = 0; a < 30; a++)
+  { // Repeat 30 times...
+    for (int b = 0; b < 3; b++)
+    {                //  'b' counts from 0 to 2...
+      strip.clear(); //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for (uint16_t c = b; c < strip.numPixels(); c += 3)
+      {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        uint16_t hue = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color);                       // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
   }
 }
